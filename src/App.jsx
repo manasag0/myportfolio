197199a005d0a3b64a5066052015011b708a838d
Manasa G <manasa.g7890@hotmail.com>
2023-11-13 22:58:14 +0530

    additional info

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-
+import skills from  './components/Skills.js'
 function App() {
 
 
@@ -8,7 +8,7 @@ function App() {
     <>
      <h1>Hello I am Manasa</h1>
      <p>Software Developer</p>
-
+    <button>My Skills</button>
     </>
   )
 }
