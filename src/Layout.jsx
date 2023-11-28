7c9b6b37d166d96fcf788e8ef02e8be594bd164c
Manasa G <manasa.g7890@hotmail.com>
2023-11-28 11:08:23 +0530

    modification

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,13 +1,14 @@
 import {} from 'react'
 // import {Outlet} from 'react-router-dom'
-import Header from './components/headers/Header'
-import Footer from './components/footers/Footer'
-import Home from './components/home/Home'
-import Skills from './components/Skills'
+// import Header from './components/headers/Header'
+// import Footer from './components/footers/Footer'
+// import Home from './components/home/Home'
+// import Skills from './components/Skills'
+import './layout.css'
 function Layout() {
   return (
-    <div>
-      <h1>Page under maintaince!!</h1>
+    <div className='container '>
+      <h1>Page under progrss!!</h1>
       {/* <Header />
       <Home />
       <Footer />
