9cdc2a8424bff85d4339ae7359ad8d26124777aa
Manasa G <manasa.g7890@hotmail.com>
2023-11-28 22:08:40 +0530

    in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 
 import './App.css'
+import Sidenav from './components/Sidenav'
 
 function App() {
 
 
   return (
-    <>
+    <div className="h-full text-white m-5 ">
+      <div>
+      <Sidenav />
       <h1>Hello world!!</h1>
-    </>
+      </div>
+      </div>
   )
 }
 
